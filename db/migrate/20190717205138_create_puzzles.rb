ff39df7c283ad175ae87a16a6b1c0f38b39be26b
Adam Sultanov <adam.sultanov@gmail.com>
2019-07-31 23:39:51 -0400

    wed-2339

--- db/migrate/20190717205138_create_puzzles.rb
+++ db/migrate/20190717205138_create_puzzles.rb
@@ -4,7 +4,7 @@ class CreatePuzzles < ActiveRecord::Migration[5.2]
       t.string :title
       t.string :description
       t.string :positions, array: true, default: []
-      t.string :moves
+      t.string :moves, array: true, default: []
       t.integer :lesson_id
       t.integer :sort_order
       t.string :style
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
 
 
-Lesson.create(id: 1, title: "the board lesson 1", description: "intro to the chess board, 5xp", order: ['1', '2', '3'], xp_worth: 5)
-
+Lesson.create(id: 1, title: "the board lesson 1", description: "intro to the chess board", order: ['1', '2', '3'], xp_worth: 5)
 Puzzle.create(id: 1, title: "first puzzle", description: "this is the very first puzzle", positions: [
   "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",
   "rnbqkbnr/pppppppp/8/8/3P4/8/PPP1PPPP/RNBQKBNR b KQkq d3 0 1",
@@ -10,32 +9,30 @@ Puzzle.create(id: 1, title: "first puzzle", description: "this is the very first
   "rnbqkbnr/pp2pppp/2p5/3p4/3PP3/8/PPP2PPP/RNBQKBNR w KQkq - 0 3",
   "rnbqkbnr/pp2pppp/2p5/3p4/3PP3/2N5/PPP2PPP/R1BQKBNR b KQkq - 1 3"
   ], moves: [], lesson_id: 1, sort_order: 1, style: "puzzle")
-Slide.create(id: 1, title: "first slide", description: "intro to the board", image: "image_url", content: "here is a bunch of content for the lesson", lesson_id: 1, sort_order: 2, style: "slide")
+Slide.create(id: 1, title: "first slide", description: "intro to the board", image: "https://images.unsplash.com/photo-1529699211952-734e80c4d42b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1651&q=80", content: "here is a bunch of content for the lesson", lesson_id: 1, sort_order: 2, style: "slide")
 Puzzle.create(id: 2, title: "second puzzle", description: "second chess board puzzle", positions: [
   "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",
   "rnbqkbnr/pppppppp/8/8/8/4P3/PPPP1PPP/RNBQKBNR w KQkq - 0 1",
   ], moves: ["e2", "e3"], lesson_id: 1, sort_order: 3, style: "test")
-Slide.create(id: 2, title: "second slide", description: "you did it!!", image: "image_url", content: "here is the page thats like congrats! you finished the first lesson", lesson_id: 1, sort_order: 4, style: "slide")
+Slide.create(id: 2, title: "second slide", description: "you did it!!", image: "https://images.unsplash.com/photo-1543092587-d8b8feaf362b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80", content: "here is the page thats like congrats! you finished the first lesson", lesson_id: 1, sort_order: 4, style: "slide")
 
-Lesson.create(id: 2, title: "the board lesson 2", description: "chess grid numbers, 10xp", order: ['3', '4', '5'], xp_worth: 10)
+Lesson.create(id: 2, title: "the board lesson 2", description: "chess grid numbers", order: ['3', '4', '5'], xp_worth: 10)
 Puzzle.create(id: 3, title: "third puzzle", description: "third chess board puzzle", positions: [
   "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",
   "rnbqkbnr/pppppppp/8/8/8/1P6/P1PPPPPP/RNBQKBNR b KQkq - 0 1"
   ], moves: "b3", lesson_id: 2, sort_order: 1, style: "puzzle")
 
-Lesson.create(id: 3, title: "the board lesson 3", description: "very good chess lesson, 15xp", order: [], xp_worth: 15)
-Slide.create(id: 3, title: "another slide", description: "you wowwow it!!", image: "image_url", content: "here is the page thats like congrats! %%you finished the first lesson%%", lesson_id: 3, sort_order: 1, style: "slide")
-Slide.create(id: 8, title: "andther slide", description: "kkkkkk i!!", image: "image_url", content: "here sdis the page %%thasdfrats!%% you finished the first lesson", lesson_id: 3, sort_order: 2, style: "slide")
-Slide.create(id: 9, title: "moreher slide", description: "youhmmmmmmmppp it!!", image: "image_url", content: "here is the page thats lsdfasdfon", lesson_id: 3, sort_order: 3, style: "slide")
+Lesson.create(id: 3, title: "the board lesson 3", description: "pointsdaf", order: [], xp_worth: 15)
+Slide.create(id: 3, title: "another slide", description: "you wowwow it!!", image: "https://images.unsplash.com/photo-1528819622765-d6bcf132f793?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80", content: "here is the page thats like congrats! you finished the first lesson", lesson_id: 3, sort_order: 1, style: "slide")
 
-Lesson.create(id: 4, title: "the board lesson 4", description: "very nice chess lesson, 15xp", order: [], xp_worth: 15)
-Slide.create(id: 4, title: "another slide", description: "you wowwow it!!", image: "image_url", content: "here is the page thats like congrats! you finished the first lesson", lesson_id: 4, sort_order: 1, style: "slide")
+Lesson.create(id: 4, title: "the board lesson 4", description: "pwozaers", order: [], xp_worth: 15)
+Slide.create(id: 4, title: "another slide", description: "you wowwow it!!", image: "https://images.unsplash.com/photo-1505461296292-7d67beed10a2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80", content: "here is the page thats like congrats! you finished the first lesson", lesson_id: 4, sort_order: 1, style: "slide")
 
-Lesson.create(id: 5, title: "lesson with tooltips 5", description: "very mouse chess lesson, 10xp", order: [], xp_worth: 15)
-Slide.create(id: 5, title: "first slide of the lesson", description: "you wowwow it!!", image: "image_url", content: "here is the page thats like %%congrats!%% you finished the first lesson", lesson_id: 5, sort_order: 1, style: "slide")
+Lesson.create(id: 5, title: "the board lesson 5", description: "points points points", order: [], xp_worth: 15)
+Slide.create(id: 5, title: "another slide", description: "you wowwow it!!", image: "https://images.unsplash.com/photo-1513159446162-54eb8bdaa79b?ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80", content: "here is the page thats like congrats! you finished the first lesson", lesson_id: 5, sort_order: 1, style: "slide")
 
-Lesson.create(id: 6, title: "the board lesson 6 an annoyingly long title", description: "very cat chess lesson and also an annoyingly long description, 25xp", order: [], xp_worth: 15)
-Slide.create(id: 6, title: "another slide", description: "you wowwow it!!", image: "image_url", content: "here is the page thats like congrats! you finished the first lesson", lesson_id: 6, sort_order: 1, style: "slide")
+Lesson.create(id: 6, title: "the board lesson 6", description: "checkers jk more chess", order: [], xp_worth: 20)
+Slide.create(id: 6, title: "another slide", description: "you wowwow it!!", image: "https://images.unsplash.com/photo-1515600467781-911b35a7e539?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80", content: "here is the page thats like congrats! you finished the first lesson", lesson_id: 6, sort_order: 1, style: "slide")
 
-Lesson.create(id: 7, title: "the board lesson 7", description: "very dog chess lesson, 25xp", order: [], xp_worth: 15)
-Slide.create(id: 7, title: "another slide", description: "you wowwow it!!", image: "image_url", content: "here is the page thats like congrats! you finished the first lesson", lesson_id: 7, sort_order: 1, style: "slide")
+Lesson.create(id: 7, title: "the board lesson 7", description: "intimidating ones opponent prior to the match", order: [], xp_worth: 20)
+Slide.create(id: 7, title: "another slide", description: "you wowwow it!!", image: "https://images.unsplash.com/photo-1559480423-a4c7efb6946a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1622&q=80", content: "here is the page thats like congrats! you finished the first lesson", lesson_id: 7, sort_order: 1, style: "slide")
